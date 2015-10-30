c7923d42330dc6a7abab92e08960ed08fc06e16f
youngfreezy <Fareez.Ahmed@gmail.com>
2015-10-29 17:28:01 -0700

    added config

---
+++ Server/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  'facebookAuth': {
+    'clientID': process.env.FACEBOOK_ID || '1689439597952787',
+    'clientSecret': process.env.FACEBOOK_SECRET ||'46f70d0b0cc3495def302e47e4f90513',
+    'callbackURL': "/auth/facebook/callback"
+  },
+
+  "dbConnection" : process.env.DB  || "localhost/seven"
+  
+};
