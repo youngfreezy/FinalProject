5b7f8b2a6fbaa518e14af2a2b2f44914006beb97
youngfreezy <Fareez.Ahmed@gmail.com>
2015-11-02 18:22:58 -0800

    fixed issue with done persistence, refactored routes/server, yummly queries

--- server.js
+++ server.js
@@ -104,7 +104,6 @@ var db = mongoose.connect(config.dbConnection, {}, function (err) {
 
 var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
