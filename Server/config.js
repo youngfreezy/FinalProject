2f9cc40ca9437f6ac272cebaad9116750fe294ba
youngfreezy <Fareez.Ahmed@gmail.com>
2015-11-03 21:25:52 -0800

    dotenv

--- Server/config.js
+++ Server/config.js
@@ -1,19 +1,4 @@
 module.exports = {
 
-  'facebookAuth': {
-    'clientID': process.env.FACEBOOK_ID || '1689439597952787',
-    'clientSecret': process.env.FACEBOOK_SECRET || '46f70d0b0cc3495def302e47e4f90513',
-    'callbackURL': "/auth/facebook/callback"
-  },
-
-  'Yummly': {
-    key: process.env.YUMMLY_KEY || 'efd918c28d5b710d9583ec24fb2bb362',
-    id: process.env.YUMMLY_ID || '3ee8ed9f'
-  },
-
-  'gmail': {
-    'pass': process.env.GMAIL || 'aeulqtlvzljhkpsd'
-  },
-
   "dbConnection": process.env.DB || "localhost/seven"
 };
