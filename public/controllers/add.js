961d3d470a498c8913e6eece3ed3335e04b74b65
youngfreezy <Fareez.Ahmed@gmail.com>
2015-11-08 14:12:13 -0800

    removed duplicate function

--- public/controllers/add.js
+++ public/controllers/add.js
@@ -1,30 +1,5 @@
 angular.module('MyApp')
   .controller('AddCtrl', function ($scope, $rootScope, $alert, $resource, $cookies, Recipes, RecipeBox) {
-    // console.log($scope.recipes);
-    // console.log($scope.quickRecipes);
-    $scope.addRecipe = function (recipe) {
-      console.log('recipe adding', recipe);
-      Recipes.save(recipe)
-        .then(function () {
-
-          $alert({
-            content: 'recipe has been added.',
-            animation: 'fadeZoomFadeDown',
-            type: 'material',
-            duration: 3
-          });
-        })
-        .catch(function (response) {
-
-          $alert({
-            content: response.data.message,
-            animation: 'fadeZoomFadeDown',
-            type: 'material',
-            duration: 3
-          });
-        });
-    };
-
     $scope.deleteRecipe = function (recipe) {
       Recipes.deleteRecipe(recipe).then(function () {
 
