eaae9862dc3c9964a89ffecc5677fd5e403e9923
youngfreezy <Fareez.Ahmed@gmail.com>
2015-11-08 19:48:53 -0800

    gave user ability to add to recipe box from individual recipe view

--- public/controllers/recipe.js
+++ public/controllers/recipe.js
@@ -1,14 +1,48 @@
 angular.module('MyApp')
-   //for individual recipes
-  .controller('RecipeCtrl', function ($scope, $routeParams, MainService) {
-    $scope.recipe = {};
-   
-   
-      MainService.getIndividualRecipe($routeParams.id).then(function (response) {
-        $scope.recipe = response;
-        // console.log(response);
-      });
+//for individual recipes
+.controller('RecipeCtrl', function ($scope, $routeParams, MainService, $rootScope, RecipeBox, $alert) {
+  $scope.recipe = {};
 
-    
-   
+
+  MainService.getIndividualRecipe($routeParams.id).then(function (response) {
+    $scope.recipe = response;
+    // console.log(response);
   });
+
+  $scope.addRecipe = function (recipe) {
+    //TODO: take this out for a better user experience.
+    if ($rootScope.currentUser === undefined) {
+      $location.path('/login');
+      return;
+    }
+
+    MainService.getIndividualRecipe(recipe.id).then(function (response) {
+      saveRecipe(response);
+      RecipeBox.recipeCount++;
+    });
+  };
+
+  //refactor this out later:
+  function saveRecipe(response) {
+    MainService.save(response)
+      .then(function (response) {
+
+        $scope.$broadcast('getUserRecipes');
+        $alert({
+          content: response.data.recipeBox ? 'recipe has been added.' : 'This recipe is already in your recipe box',
+          animation: 'fadeZoomFadeDown',
+          type: 'material',
+          duration: 1
+        });
+      })
+      .catch(function (response) {
+        $alert({
+          content: response.data.message,
+          animation: 'fadeZoomFadeDown',
+          type: 'material',
+          duration: 3
+        });
+      });
+  }
+
+});
